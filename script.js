9a2a90412c816704c3c298224461470e82333975
testeurhwek <khalilmra@yahoo.com>
2026-02-07 00:17:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+const form = document.getElementById("memoryForm");
+const formSection = document.getElementById("formSection");
+const valentineSection = document.getElementById("valentineSection");
+
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const message = document.getElementById("message");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const date = document.getElementById("meetDate").value;
+    const place = document.getElementById("meetPlace").value;
+
+    if (!date || !place) return;
+
+    formSection.classList.add("hidden");
+    valentineSection.classList.remove("hidden");
+
+    message.textContent = `💕 Since ${date}, at ${place}...`;
+});
 
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * 200 - 100;
@@ -9,6 +27,6 @@ noBtn.addEventListener("mouseover", () => {
 });
 
 yesBtn.addEventListener("click", () => {
-    message.textContent = "Yaaaay 💕 I knew it💖";
-
+    message.textContent = "💍 You just made me the happiest person 💖";
 });
+
